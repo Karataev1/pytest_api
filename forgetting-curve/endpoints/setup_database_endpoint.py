1dc2daa84b6d82419dfacb29c89a826c87ba6f9a
Karataev <ivan.karataev19@gmail.com>
2025-06-18 09:22:57 +0300

    first commit

--- forgetting-curve/endpoints/setup_database_endpoint.py
+++ forgetting-curve/endpoints/setup_database_endpoint.py
@@ -1,5 +1,5 @@
 from endpoints.base_endpoint import BaseEndpoint
-
+import allure
 
 class SetupDataBaseEndpoint(BaseEndpoint):
     def __init__(self):
@@ -7,6 +7,7 @@ class SetupDataBaseEndpoint(BaseEndpoint):
         self.endpoint_name = 'setup_database'
 
 
+    @allure.step('Отправляю запрос на создание базы данных')
     def setup_database(self) -> 'SetupDataBaseEndpoint':
         """
         Метод отправляет запрос на создание базы данных
--- forgetting-curve/endpoints/users_endpoint.py
+++ forgetting-curve/endpoints/users_endpoint.py
@@ -1,5 +1,6 @@
 from endpoints.base_endpoint import BaseEndpoint
 import random
+import allure
 
 
 class UserEndpoint(BaseEndpoint):
@@ -8,6 +9,7 @@ class UserEndpoint(BaseEndpoint):
         self.endpoint_name = 'users'
 
 
+    @allure.step('Запрашиваю список пользователей')
     def get_user(self,nickname:str ='') -> dict:
         """
         Метод для получения списка пользователей, либо одного конкретного,
@@ -18,6 +20,7 @@ class UserEndpoint(BaseEndpoint):
         return self.get_json()
 
 
+    @allure.step('Создаю пользователя')
     def create_user(self,payload: dict) -> 'UserEndpoint':
         """
         Метод отправляет запрос на создание пользователя,
@@ -27,6 +30,7 @@ class UserEndpoint(BaseEndpoint):
         return self
 
 
+    @allure.step('Обновляю данные пользователя')
     def update_user(self,nickname: str, payload: dict) -> 'UserEndpoint':
         """
         Метод отправляет запрос на обновление данных пользователя
@@ -35,6 +39,7 @@ class UserEndpoint(BaseEndpoint):
         return self
 
 
+    @allure.step('Удаляю пользователя')
     def delete_user(self,nickname: str) -> 'UserEndpoint':
         """
         Метод отправляет запрос на удаление пользователя
@@ -43,6 +48,7 @@ class UserEndpoint(BaseEndpoint):
         return self
 
 
+    @allure.step('Получаю случайного пользователя')
     def get_random_user(self) -> dict:
         """
         Метод возвращает случайного пользователя из базы данных
